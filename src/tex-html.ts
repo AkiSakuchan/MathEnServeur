9f766d7190d0a0d851ca5fe5ad8f8cf70e63dcbe
gheougje <rou.leon@icloud.com>
2023-07-19 15:53:57 +0200

    小修改

--- src/tex-html.ts
+++ src/tex-html.ts
@@ -10,29 +10,23 @@ interface Request
 }
 
 let server = net.createServer();
+let sockets : Array<net.Socket> = [];   // 用于记录以连接的套接字, 以便推出时能清理干净
+
 server.on('connection', (socket) => {
     console.log('连接建立');
+    sockets.push(socket);
 
     let recu = '';
     socket.on('data', (data:string) =>{
-        // 因为并不能假设data中的数据是完整的, 因此约定以一个A字符为结尾
+        // 并不能假设data中的数据是完整的
         recu += data;
-        // 判断是否接受完所有数据
-        if( data.toString().endsWith('A') )
-        {
-            recu = recu.slice(0, recu.length -1);   // 去除结尾的A避免JSON解析错误
-        }
-        else
-        {
-            console.log('收到部分数据');
-            return;
-        }
+    });
 
+    socket.on('end', () => {
         let source:Request;
         try
         {
             source = JSON.parse(recu) as Request;
-            recu = '';
         }
         catch(err)
         {
@@ -40,7 +34,6 @@ server.on('connection', (socket) => {
             {
                 // 避免JSON语法错误把程序搞崩溃
                 console.log('JSON语法错误:'+err.message);
-                recu = '';
                 return;
             }
             else
@@ -52,7 +45,7 @@ server.on('connection', (socket) => {
         {
             let result = katex.renderToString( source.tex, {displayMode: source.display, output: 'html' });
             console.log(Date.now());
-            socket.write(result);
+            socket.end(result);
         }
         catch(err)
         {
@@ -61,15 +54,24 @@ server.on('connection', (socket) => {
                 // 避免 TeX 语法错误把程序搞崩溃
                 let result = err.message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
                 recu = '';
-                socket.write(result);
+                socket.end(result);
             }
             else
             {
                 console.log('错误', err);
-                throw err;
             }
         }
     });
+
+    socket.on('close', (hadError) =>
+    {
+        sockets.splice(sockets.indexOf(socket), 1);
+        if(!hadError)
+        {
+            console.log('连接正常关闭');
+        }
+    })
+    
     socket.on('error', (err) => {
         console.log('错误:',err.message);
     })
@@ -81,12 +83,22 @@ server.on('error', (err:NodeJS.ErrnoException)=>{
         console.log('套接字 ' + socketPath + ' 已被占用, 请换一个路径或删除对应套接字文件');
         process.exit(1);
     }
-})
+});
 server.listen(socketPath, ()=> console.log('监听中'));
 
 // 手动结束进程时需要关闭套接字
 process.on('SIGINT', ()=>{
     console.log('正在结束进程');
     server.close();
+
+    // 强制关闭已经连接的套接字, 避免阻碍正常退出
+    sockets.forEach((socket) => {
+        socket.destroy();
+    });
 });
-process.on('SIGTERM', ()=>{server.close();});
+process.on('SIGTERM', ()=>{
+    server.close();
+    sockets.forEach((socket) => {
+        socket.destroy();
+    });
+});
